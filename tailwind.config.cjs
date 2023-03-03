c07253c5507fe65db786126beb6b0c3b51bc1830
Bytemaster82 <tonylian1224@gmail.com>
2023-03-03 14:28:43 +1100

    add colours to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { primaryBlue: "#6d66fa", accentBlue: "#3f3d55" },
+    },
   },
   plugins: [],
 };
