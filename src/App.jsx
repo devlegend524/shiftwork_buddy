a463f345e7fd11bc21e92202599bbede0a283b9a
Bytemaster82 <tonylian1224@gmail.com>
2023-03-07 13:16:04 +1100

    implement react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import { UserContext } from "./context/UserContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Shifts from "./pages/Shifts/Shifts";
+import Account from "./pages/Account";
 
 function App() {
   const [user, setUser] = useState();
+  const [shifts, setShifts] = useState();
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {!user ? <Login /> : <Home />}
+    <UserContext.Provider value={{ user, setUser, shifts, setShifts }}>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Shifts />} />
+              <Route path='/account' element={<Account />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      )}
     </UserContext.Provider>
   );
 }
