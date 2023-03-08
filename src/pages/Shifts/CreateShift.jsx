f0fdd9c173f6ec49f7e92fc8545fa9e2c85b3247
Bytemaster82 <tonylian1224@gmail.com>
2023-03-08 20:07:19 +1100

    form input component for CreateShift

--- src/pages/Shifts/CreateShift.jsx
+++ src/pages/Shifts/CreateShift.jsx
@@ -2,7 +2,8 @@ import React, { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { dropDownData } from "../../utils/ButtonData";
 
-import DropdownButton from "../../components/DropdownButton";
+import FormButton from "../../components/FormButton";
+import FormInput from "../../components/FormInput";
 
 function CreateShift() {
   const { setShifts } = useContext(UserContext);
@@ -11,31 +12,48 @@ function CreateShift() {
   return (
     <div className='absolute h-[700px] w-full top-0 left-0 p-5'>
       <form className='h-full w-full bg-white rounded-lg p-5'>
-        <h1>Create Shift</h1>
+        <h1 className='font-semibold text-xl'>Create Shift</h1>
 
         <div className='flex flex-col gap-3 pt-5'>
-          <DropdownButton
+          <FormButton
             inputForm={inputForm}
             setInputForm={setInputForm}
             dataObject={dropDownData.days}
-            query={"Day"}
+            query={"day"}
             id={"days-menu-container"}
           />
-          <DropdownButton
+          <FormButton
             inputForm={inputForm}
             setInputForm={setInputForm}
             dataObject={dropDownData.dates}
-            query={"Date"}
+            query={"date"}
             id={"dates-menu-container"}
           />
-          <DropdownButton
+          <FormButton
             inputForm={inputForm}
             setInputForm={setInputForm}
             dataObject={dropDownData.months}
-            query={"Month"}
+            query={"month"}
             id={"months-menu-container"}
           />
         </div>
+
+        <h2 className='font-semibold pt-5'>
+          Enter your start and finish time:
+        </h2>
+
+        <FormInput
+          id={"start"}
+          placeHolder={"Start time: 1445"}
+          inputForm={inputForm}
+          setInputForm={setInputForm}
+        />
+        <FormInput
+          id={"finish"}
+          placeHolder={"Finish time: 2300"}
+          inputForm={inputForm}
+          setInputForm={setInputForm}
+        />
       </form>
     </div>
   );
