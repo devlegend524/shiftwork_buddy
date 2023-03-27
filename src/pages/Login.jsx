fb1156cd5d03fd9d8974c70cbd8cf52237329da2
Bytemaster82 <tonylian1224@gmail.com>
2023-03-27 22:10:14 +1100

    modify login UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,21 +29,21 @@ function Login() {
     loginContainer:
       "flex items-center justify-center h-screen lg:justify-evenly mx-5",
     innerContainer: "text-center",
-    heading: "text-4xl font-semibold lg:min-w-[500px] lg:text-6xl",
-    subHeading: "pt-3 lg:text-2xl",
+    heading: "text-4xl font-semibold lg:min-w-[200px] lg:text-5xl",
+    subHeading: "pt-3 lg:text-xl",
     buttonImage: "mr-3 w-[30px]",
     googleButton:
-      "text-white bg-primaryBlue hover:bg-accentBlue focus:ring-4 focus:outline-none focus:ring-gray-100 rounded-lg font-semibold px-5 py-2.5 items-center inline-flex justify-center mb-2 w-full mt-10 lg:text-lg",
+      "text-white bg-primaryBlue hover:bg-accentBlue focus:ring-4 focus:outline-none focus:ring-gray-100 rounded-lg font-semibold px-5 py-2.5 items-center inline-flex justify-center mb-2 w-full mt-10",
     githubButton:
-      "text-white bg-primaryBlue hover:bg-accentBlue focus:ring-4 focus:outline-none focus:ring-gray-100 rounded-lg font-semibold px-5 py-2.5 items-center inline-flex justify-center mb-2 w-full mt-3 lg:text-lg",
-    loginArt: "hidden lg:block max-w-[800px]",
+      "text-white bg-primaryBlue hover:bg-accentBlue focus:ring-4 focus:outline-none focus:ring-gray-100 rounded-lg font-semibold px-5 py-2.5 items-center inline-flex justify-center mb-2 w-full mt-3",
+    loginArt: "hidden lg:block max-w-[500px]",
   };
 
   return (
     <div className={style.loginContainer}>
       <div className={style.innerContainer}>
         <h1 className={style.heading}>Shiftwork Buddy</h1>
-        <p className={style.subHeading}>Your tracking and income assistant.</p>
+        <p className={style.subHeading}>Your shiftwork assistant.</p>
 
         <div>
           <button
