2660459bed360a6f680ed068d6075a13d12ca057
Bytemaster82 <tonylian1224@gmail.com>
2023-04-01 18:36:14 +1100

    Greeting page UI changes

--- src/pages/Shifts/Greeting.jsx
+++ src/pages/Shifts/Greeting.jsx
@@ -3,7 +3,7 @@ import { UserContext } from "../../context/UserContext";
 
 const style = {
   container:
-    "absolute flex flex-col items-center justify-center w-full top-[50%] p-5",
+    "absolute flex flex-col items-center justify-center w-full top-[35%] p-5",
   h2: "text-2xl text-center font-semibold",
   lowerContainer: "text-center mt-5 md:text-lg",
   boldH: "text-[#6d66fa]",
